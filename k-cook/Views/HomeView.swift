0889c5d574891663ccb0b051283512ea8977b72d
joao <jpedros1511@gmail.com>
2025-03-20 17:51:09 -0300

    Estruturando componentes homepage

--- k-cook/Views/HomeView.swift
+++ k-cook/Views/HomeView.swift
@@ -1,80 +1,138 @@
-//
-//  Home.swift
-//  k-cook
-//
-//  Created by found on 18/03/25.
-//
-
 import SwiftUI
 
 struct HomeView: View {
-    @State private var searchText = ""
+    @State private var showingSheet = false
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 20) {
             HStack {
                 Text("K-COOK")
-                    .font(.largeTitle)
-                    .fontDesign(.monospaced)
-                    .bold()
-                    .padding()
+                    .font(.title)
+                    .fontWeight(.bold)
                     .foregroundColor(.red)
-                    .offset(x: 0, y: 0)
-
-                HStack {
-                    NavigationStack {
-                        Text(" \(searchText)")
-                        Spacer()
-                    }
-                }
-
-                VStack {
-                    Button(action: {
-                        print("Selecionado")
-                    }) {
+                    .fontDesign(.monospaced)
+                Spacer()
+            }
+            .offset(x:15, y:-120)
+        }
+        
+        VStack(alignment: .leading, spacing: 20) {
+            Text("O que vamos preparar?")
+                .font(.custom("Poppins-Bold", size: 25))
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .fontDesign(.default)
+                .offset(x:15, y:-69)
+            
+            Text("Hora de cozinhar!")
+                .font(.custom("Poppins-Bold", size: 25))
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .fontDesign(.default)
+                .offset(x:15, y:-150)
+        }
+        .offset(x:-65, y:-0)
+        
+        VStack{
+            HStack(spacing: 40){
+                Button(action: {
+                    print("Teste")
+                }) {
+                    VStack{
+                        Image(systemName: "cup.and.saucer.fill")
                         Text("Café da manhã")
-
-                            .foregroundColor(.black)
-                            .background(.white)
-                            .cornerRadius(20)
-                            .shadow(radius: 2)
                     }
-                    Button(action: {
-                        print("Selecionado")
-                    }) {
+                    .border(Color.black, width: 1)
+                }
+                Button(action: {
+                    print("Teste")
+                }) {
+                    VStack {
+                        Image(systemName: "cup.and.saucer.fill")
                         Text("Almoço")
-
-                            .foregroundColor(.black)
-                            .background(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 2)
                     }
-                    Button(action: {
-                        print("Selecionado")
-                    }) {
+                    .border(Color.black, width: 1)
+                }
+                Button(action: {
+                    print("Teste")
+                }) {
+                    VStack {
+                        Image(systemName: "sunrise.fill")
                         Text("Jantar")
-
-                            .foregroundColor(.black)
-                            .background(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 2)
                     }
-                    Button(action: {
-                        print("selecionado")
-                    }) {
-                        Text("Sobremesa")
-
-                            .foregroundColor(.black)
-                            .background(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 2)
-
-                    }
+                    .border(Color.black, width: 1)
                 }
             }
+            .offset(x:0, y:-100)
         }
+            
+            
+            
+    
+            VStack{
+                Text("Receitas")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .fontDesign(.default)
+                    .offset(x:-124, y:20)
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 20){
+                        ForEach(0..<5){_ in
+                            Image(systemName: "star.fill")
+                        }
+                        Text("aaaaaaa")
+                        Text("bbbbbbb")
+                        Text("ccccccc")
+                        
+                    }
+                }
+                .padding()
+            }
+            .offset(x:-1, y:0)
+            
+            VStack{
+                Text("Doramas")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .fontDesign(.default)
+                    .offset(x:-124, y:20)
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 20){
+                        Image(systemName: "star.fill")
+                        Text("aaaaaaa")
+                        Text("bbbbbbb")
+                        Text("ccccccc")
+                    }
+                }
+                .padding()
+            }
+            .offset(x:-1, y:0)
+            
+            VStack{
+                Text("Histórias")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .fontDesign(.default)
+                    .offset(x:-124, y:20)
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 20){
+                        Image(systemName: "star.fill")
+                        Text("aaaaaaa")
+                        Text("bbbbbbb")
+                        Text("ccccccc")
+                    }
+                }
+                .padding()
+            }
+        
     }
 }
-
 #Preview {
     HomeView()
 }
