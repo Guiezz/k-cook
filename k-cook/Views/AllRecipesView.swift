1ebee6811a094d01a3c093571846b563ba7ac83b
Guilherme Bessa <guibessa@alu.ufc.br>
2025-03-21 11:07:59 -0300

    ajustes no codigo da home

---
+++ k-cook/Views/AllRecipesView.swift
@@ -0,0 +1,18 @@
+//
+//  AllRecipesView.swift
+//  k-cook
+//
+//  Created by User on 21/03/25.
+//
+
+import SwiftUI
+
+struct AllRecipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllRecipesView()
+}
