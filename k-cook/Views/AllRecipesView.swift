f016a014c479aeb455670a6958354fd480b0169c
Guilherme Bessa <guibessa@alu.ufc.br>
2025-03-21 15:14:57 -0300

    adicionando funcionalide de ver todas as receitas

--- k-cook/Views/AllRecipesView.swift
+++ k-cook/Views/AllRecipesView.swift
@@ -1,18 +1,51 @@
-//
-//  AllRecipesView.swift
-//  k-cook
-//
-//  Created by User on 21/03/25.
-//
-
 import SwiftUI
 
 struct AllRecipesView: View {
+    @EnvironmentObject var dataManager: DataManager
+    @State private var selectedReceita: Receita?
+
+    @Environment(\.dismiss) private var dismiss
+
+    private let columns = [
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0),
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical) {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(dataManager.receitas) { receita in
+                    RecipeCardView(receita: receita)
+                        .aspectRatio(0.7, contentMode: .fit)
+                        .onTapGesture {
+                            selectedReceita = receita
+                        }
+                }
+            }
+            .padding(.vertical)
+        }
+        .sheet(item: $selectedReceita) { receita in
+            RecipeDetailSheet(receita: receita)
+        }
+        .navigationTitle("Todas as receitas")
+        .navigationBarTitleDisplayMode(.large)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Label("Back", systemImage: "arrow.left")
+                }
+
+            }
+        }
     }
+
 }
 
 #Preview {
+
     AllRecipesView()
+        .environmentObject(DataManager())
 }
--- k-cook/Views/HomeView.swift
+++ k-cook/Views/HomeView.swift
@@ -5,100 +5,105 @@ struct HomeView: View {
     @State private var selectedReceita: Receita?
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
-                HStack {
-                    Text("K-COOK")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.red)
-                        .fontDesign(.monospaced)
-                    Spacer()
-                }
-                .padding(.horizontal)
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    HStack {
+                        Text("K-COOK")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.red)
+                            .fontDesign(.monospaced)
+                        Spacer()
+                    }
+                    .padding(.horizontal)
 
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("O que vamos preparar?")
-                        .font(.title2)
-                        .fontWeight(.bold)
+                    // Títulos
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Hora de cozinhar!")
+                            .font(.title2)
+                            .fontWeight(.bold)
 
-                    Text("Hora de cozinhar!")
-                        .font(.title3)
-                        .foregroundColor(.secondary)
-                }
-                .padding(.horizontal)
-                ScrollView(.horizontal, showsIndicators: false) {
+                        Text("O que vamos preparar?")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                    }
+                    .padding(.horizontal)
 
-                    HStack(spacing: 10) {
-                        ForEach(
-                            ["Café da manhã", "Almoço", "Jantar", "Sobremesa"],
-                            id: \.self
-                        ) { refeicao in
-                            Button(action: {
-                                // Adicionar ações de filtro dps
-                            }) {
-                                VStack {
-                                    Text(refeicao)
-                                        .font(.caption)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 10) {
+                            ForEach(
+                                [
+                                    "Café da manhã", "Almoço", "Jantar",
+                                    "Sobremesa",
+                                ],
+                                id: \.self
+                            ) { refeicao in
+                                Button(action: {
+                                    // Adicionar ações de filtro dps
+                                }) {
+                                    VStack {
+                                        Text(refeicao)
+                                            .font(.caption)
+                                    }
+                                    .padding()
+                                    .background(Color(.systemBackground))
+                                    .cornerRadius(10)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(.blue, lineWidth: 5)
+                                    )
                                 }
-
-                                .padding()
-                                .background(Color(.systemBackground))
-                                .cornerRadius(10)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(.blue, lineWidth: 5)
-                                )
-
                             }
                         }
+                        .padding()
                     }
                     .padding()
-                }
-                .padding()
 
-                VStack(alignment: .leading) {
-                    Text("Receitas")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .padding(.horizontal)
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Receitas")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .padding(.horizontal)
+                            Spacer()
 
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 15) {
-                            ForEach(dataManager.receitas) { receita in
-                                RecipeCardView(receita: receita)
-                                    .frame(width: 147)
-                                    .onTapGesture {
-                                        selectedReceita = receita
-                                    }
+                            NavigationLink(destination: AllRecipesView()) {
+                                Text("Ver tudo")
+                                    .foregroundColor(.red)
+                                    .font(.callout)
+                                    .padding(.horizontal)
                             }
                         }
-                        .padding(.horizontal)
+
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 15) {
+                                ForEach(dataManager.receitas) { receita in
+                                    RecipeCardView(receita: receita)
+                                        .frame(width: 147)
+                                        .onTapGesture {
+                                            selectedReceita = receita
+                                        }
+                                }
+                            }
+                            .padding(.horizontal)
+                        }
                     }
                 }
+                .padding(.vertical)
             }
-            .padding(.vertical)
-        }
-        .sheet(item: $selectedReceita) { receita in
-            RecipeDetailSheet(receita: receita)
-        }
-    }
-
-    private func refeicaoIcon(for refeicao: String) -> String {
-        switch refeicao {
-        case "Café da manhã": return "cup.and.saucer.fill"
-        case "Almoço": return "fork.knife"
-        case "Jantar": return "sunset.fill"
-        case "Sobremesa": return "apple.logo"
-        default: return "questionmark"
+            .sheet(item: $selectedReceita) { receita in
+                RecipeDetailSheet(receita: receita)
+            }
         }
     }
 }
 
 #Preview {
-    let dataManager = DataManager()
-    dataManager.loadData()
 
-    return HomeView()
-        .environmentObject(dataManager)
+    HomeView()
+           .environmentObject(DataManager())
+
 }
