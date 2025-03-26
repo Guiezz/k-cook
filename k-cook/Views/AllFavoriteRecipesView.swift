7252ddc478efe6dcde419260bfe1a5631b5c0ad1
Guilherme Bessa <guibessa@alu.ufc.br>
2025-03-26 15:03:20 -0300

    adicionando Splash screen

---
+++ k-cook/Views/AllFavoriteRecipesView.swift
@@ -0,0 +1,18 @@
+//
+//  AllFavoriteRecipesView.swift
+//  k-cook
+//
+//  Created by User on 26/03/25.
+//
+
+import SwiftUI
+
+struct AllFavoriteRecipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllFavoriteRecipesView()
+}
---
+++ k-cook/Views/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  k-cook
+//
+//  Created by User on 26/03/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
